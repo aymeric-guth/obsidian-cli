b208295cbdc74a7df281ed91138cb18542bbb141
yul <aymeric.guth@protonmail.com>
2022-11-06 21:50:09 +0100

    external func tag_finder

--- obsidian_cli/obsidian_cli.py
+++ obsidian_cli/obsidian_cli.py
@@ -11,18 +11,16 @@ import time
 import re
 from utils import cli
 import utils
+from .core import tag_parser
 
 
 URI_OPEN = "obsidian://open?vault={vault_id}&file={filename}"
 URI_SEARCH = "obsidian://search?vault={vault_id}&query={query}"
 USER_HOME = os.getenv("HOME")
 if not USER_HOME:
-    raise RuntimeError("can you please fuck off")
+    raise RuntimeError("can you kindly fuck off, sir")
 OBSIDIAN_DIR_DARWIN = f"{USER_HOME}/Library/Application Support/obsidian"
 OBSIDIAN_DIR_LINUX = f"{USER_HOME}/.config/obsidian"
-
-
-env = cli.Environment()
 
 
 def obsidian_encode(param: str, meta: str = "") -> str:
@@ -30,17 +28,15 @@ def obsidian_encode(param: str, meta: str = "") -> str:
 
 
 def to_obsidian_root(path: pathlib.PurePath | pathlib.Path) -> str:
-    global env
-
-    return str(path)[len(str(env.get("OBSIDIAN_VAULT"))) :]  # type: ignore
+    return str(path)[len(str(cli.env.get("OBSIDIAN_VAULT"))) :]
 
 
 def check_env() -> tuple[str, int]:
-    (msg, defined) = env.query("OBSIDIAN_DIR")
+    (msg, defined) = cli.env.query("OBSIDIAN_DIR")
     if sys.platform == "darwin" and not defined:
-        env.set("OBSIDIAN_DIR", OBSIDIAN_DIR_DARWIN)
+        cli.env.set("OBSIDIAN_DIR", OBSIDIAN_DIR_DARWIN)
     elif sys.platform == "linux" and not defined:
-        env.set("OBSIDIAN_DIR", OBSIDIAN_DIR_LINUX)
+        cli.env.set("OBSIDIAN_DIR", OBSIDIAN_DIR_LINUX)
     else:
         return cli.failure("i've got bad news for you bud")
 
@@ -49,11 +45,11 @@ def check_env() -> tuple[str, int]:
     # var defined ?
     # no -> return error
     # yes -> next step
-    (msg, defined) = env.query("OBSIDIAN_VAULT")
+    (msg, defined) = cli.env.query("OBSIDIAN_VAULT")
     if not defined:
         return cli.failure(msg)
 
-    obsidian_dir = pathlib.Path(env.get("OBSIDIAN_DIR"))
+    obsidian_dir = pathlib.Path(cli.env.get("OBSIDIAN_DIR"))
     obsidian_cfg = obsidian_dir / "obsidian.json"
     if not obsidian_cfg.exists():
         return cli.failure(f"{obsidian_cfg=} not found")
@@ -65,47 +61,74 @@ def check_env() -> tuple[str, int]:
     if vaults is None:
         return cli.failure("no vaults are registred")
 
-    (msg, defined) = env.query("STACK_FILE")
+    (msg, defined) = cli.env.query("STACK_FILE")
     if not defined:
-        env.set("STACK_FILE", "300 Resources/Stack.md")
+        cli.env.set("STACK_FILE", "300 Resources/Stack.md")
 
     for k, v in vaults.items():
-        if v.get("path", "") == env.get("OBSIDIAN_VAULT"):
-            env.set("VAULT_ID", k)
+        if v.get("path", "") == cli.env.get("OBSIDIAN_VAULT"):
+            cli.env.set("VAULT_ID", k)
             return cli.success()
 
     return cli.failure("vault is not active")
 
 
-def parse_args(cmd: list[str]) -> tuple[str, int]:
-    files: list[pathlib.PurePath] = lsfiles.iterativeDFS(
-        lambda x: (
-            lsfiles.Maybe.unit(x)
-            .bind(lsfiles.filters.dotfiles)
-            .bind(
-                lsfiles.filters.ext(
-                    {
-                        ".md",
-                    }
+def tag_finder(msg: str) -> tuple[str, int]:
+    def catcher(fnc):
+        def inner(*args, **kwargs):
+            try:
+                return fnc(*args, **kwargs).get("tags")
+            except Exception as err:
+                raise RuntimeError(err, *args, **kwargs)
+
+        return inner
+
+    return cli.success(
+        "\n".join(
+            [
+                t
+                for f in (
+                    lsfiles.iterativeDFS(
+                        filters=lsfiles.filters.ext(
+                            {
+                                ".md",
+                            }
+                        ),
+                        adapter=pathlib.PurePath,
+                        root=pathlib.Path(cli.env.get("OBSIDIAN_VAULT")),
+                    )
                 )
-            )
-        ),
-        pathlib.PurePath,
-        pathlib.Path(env.get("OBSIDIAN_VAULT")),
+                if (t := catcher(tag_parser)(lambda f: lambda: open(f).read())(f))
+                is not None
+            ]
+        )
     )
 
-    pdb.set_trace()
+
+def parse_args(cmd: list[str]) -> tuple[str, int]:
+    files: list[pathlib.PurePath] = lsfiles.iterativeDFS(
+        filters=lsfiles.filters.ext(
+            {
+                ".md",
+            }
+        ),
+        adapter=pathlib.PurePath,
+        root=pathlib.Path(cli.env.get("OBSIDIAN_VAULT")),
+    )
+
     match cmd:
         case []:
             return cli.success()
 
         case ["open" | "o"]:
-            resource = pathlib.Path(env.get("OBSIDIAN_VAULT")) / env.get("STACK_FILE")
+            resource = pathlib.Path(cli.env.get("OBSIDIAN_VAULT")) / cli.env.get(
+                "STACK_FILE"
+            )
             if not resource.exists():
                 return cli.failure(f"{resource=} could not be found on disk")
             uri = URI_OPEN.format(
-                vault_id=env.get("VAULT_ID"),
-                filename=obsidian_encode(env.get("STACK_FILE")),
+                vault_id=cli.env.get("VAULT_ID"),
+                filename=obsidian_encode(cli.env.get("STACK_FILE")),
             )
             subprocess.run(["open", uri])
             return cli.success("open | o")
@@ -116,7 +139,7 @@ def parse_args(cmd: list[str]) -> tuple[str, int]:
             if not f:
                 return cli.failure(f"no match for: {file=}")
             uri = URI_OPEN.format(
-                vault_id=env.get("VAULT_ID"),
+                vault_id=cli.env.get("VAULT_ID"),
                 filename=obsidian_encode(to_obsidian_root(files[0])),
             )
             subprocess.run(["open", uri])
@@ -125,7 +148,7 @@ def parse_args(cmd: list[str]) -> tuple[str, int]:
         case ["find" | "f", query]:
             if "#" in query:
                 uri = URI_SEARCH.format(
-                    vault_id=env.get("VAULT_ID"),
+                    vault_id=cli.env.get("VAULT_ID"),
                     query=obsidian_encode(meta="tag:", param=query),
                 )
                 subprocess.run(["open", uri])
@@ -133,6 +156,15 @@ def parse_args(cmd: list[str]) -> tuple[str, int]:
 
             else:
                 return parse_args(["o", *cmd[1:]])
+
+        case ["match" | "m"]:
+            return cli.failure()
+            # [
+            #    t
+            #    for f in files
+            #    if (t := catcher(tag_parser)(lambda f: lambda: open(f).read())(f))
+            #    is not None
+            # ]
 
         case ["find" | "f"]:
             return cli.failure("usage: find | f tag[/sub-tag] | filename")
@@ -154,7 +186,9 @@ def main(*args) -> tuple[str, int]:
         subprocess.run(
             [
                 "open",
-                "obsidian://open?vault={vault_id}".format(vault_id=env.get("VAULT_ID")),
+                "obsidian://open?vault={vault_id}".format(
+                    vault_id=cli.env.get("VAULT_ID")
+                ),
             ]
         )
         c = 0
@@ -176,38 +210,8 @@ def _main() -> int:
     return cli.sh_fnc(main)(sys.argv[1:])
 
 
-def mdman(msg: str) -> tuple[str, int]:
+def _tag_finder() -> int:
     (msg, ok) = check_env()
     if not ok:
-        return cli.failure(msg)
-
-    def catcher(fnc):
-        def inner(*args, **kwargs):
-            try:
-                return fnc(*args, **kwargs).get("tags")
-            except Exception as err:
-                raise RuntimeError(err, *args, **kwargs)
-
-        return inner
-
-    from .core import tag_parser
-
-    res = [
-        (f, t)
-        for f, t in (
-            (f, catcher(tag_parser)((lambda f: lambda: open(f).read())(f)))
-            for f in lsfiles.iterativeDFS(
-                lsfiles.filters.ext({".md"}),
-                lambda f: f,
-                pathlib.Path(env.get("OBSIDIAN_VAULT")),
-            )
-        )
-        if t is not None
-    ]
-    print(len(res))
-
-    return cli.success()
-
-
-def _mdman() -> int:
-    return cli.sh_fnc(mdman)("")
+        return cli.sh_fnc(cli.failure)(msg)
+    return cli.sh_fnc(tag_finder)("")
